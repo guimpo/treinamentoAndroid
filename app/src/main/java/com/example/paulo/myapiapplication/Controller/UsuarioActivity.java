cf222a0ca825697fb6a60b557cecdac691a271aa
Paulo Nakaima <nakaima.prk@gmail.com>
2018-11-12 11:32:42 -0200

    Add UsuarioActivity

---
+++ app/src/main/java/com/example/paulo/myapiapplication/Controller/UsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.paulo.myapiapplication.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.paulo.myapiapplication.R;
+
+public class UsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
